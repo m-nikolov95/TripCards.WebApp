aef1653fc103d47f3f11bebaa08a9a0cdc103d43
m-nikolov95 <m-nikolov95@users.noreply.github.com>
2026-02-10 19:15:09 +0200

    Created LoadingSpinnerComponent

---
+++ src/components/shared/index.ts
@@ -0,0 +1,7 @@
+import { ErrorAlertComponent } from './error-alert-component/ErrorAlertComponent';
+import { LoadingSpinnerComponent } from './loading-spinner-component/LoadingSpinnerComponent';
+
+export {
+    ErrorAlertComponent,
+    LoadingSpinnerComponent
+}
